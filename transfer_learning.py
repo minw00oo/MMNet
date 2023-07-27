0034ddbd45262ba5e79e376c26fdb5aa9d27e7eb
minw00oo <127107965+minw00oo@users.noreply.github.com>
2023-07-27 15:09:14 +0900

    Create transfer_learning.py

---
+++ transfer_learning.py
@@ -0,0 +1,18 @@
+from tensorflow.keras import Model, callbacks, optimizers
+from tensorflow.keras.models import load_model
+
+a = 'after first_encoder_block'
+b = 'befor last_decoder_block'
+
+model = load_model('MNet5.h5')
+model.summary()
+
+for layer in model.layers[a:b]:
+    layer.trainable = False
+model.summary()
+
+adam = optimizers.Adam(lr=1e-3, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+
+model.compile(optimizer=adam,
+              loss="mse",
+              metrics=["mse"])
